5ee99e164cfcbdaba57c5b3f981edc66b5feeeba
eyal gerby <eyalg535@gmail.com>
2023-04-10 21:36:33 -0400

    commit tree 52f5b9cf27765a7f8598ad7d76f468ff056a83a6

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,35 @@
+class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    def index
+        campers = Camper.all
+        render json: campers.to_json(only: [:id, :name, :age])
+      end
+    
+      def show
+        camper = Camper.find(params[:id])
+        render json: camper
+      end
+
+      def create
+        camper = Camper.create!(camper_params)
+        render json: camper.to_json(only: [:id, :name, :age]), status: :created
+      end
+
+      private
+
+      def camper_params
+        params.permit(:name, :age)
+      end
+
+      def record_not_found
+        render json: { error: "Camper not found" }, status: :not_found
+      end
+
+      def record_invalid(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+      end
+    
+    
+end
